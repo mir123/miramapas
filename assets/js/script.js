8a12cf0d03c2b61eb97fc9aa86d3da1196668da1
mir123 <mir123@gmail.com>
2024-09-11 18:14:30 -0600

    Más avances

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
   "use strict";
+
+  const defaultLanguage = "es";
+
+  let langCookie = getCookie("siteLanguage");
+
+  if (!langCookie) {
+    setCookie("siteLanguage", defaultLanguage);
+    langCookie = defaultLanguage;
+  }
+
+  if (window.location.pathname === "/") {
+    window.location.href = "/" + langCookie;
+  }
+
   // Scroll to top
   $("a[href='#top']").click(function () {
     $("html, body").animate({ scrollTop: 0 }, "slow");
@@ -29,25 +43,69 @@ $(document).ready(function () {
     $(".site-testimonial-item").removeClass("active");
   });
 
-  //   const textContainer = document.querySelector(".tituloFondo");
-  //   const text = textContainer.textContent.trim();
-  //   const words = text.split(/\s+/);
-  //   textContainer.innerHTML = words
-  //     .map((word) => `<span>${word}</span>`)
-  //     .join(" ");
+  console.log("haciendo el onclick");
+
+  $("#botonLenguaje").on("click", function (event) {
+    console.log("clic en boton");
+    const lang = $(this).data("lang");
+    // const currentLang = getCookie("siteLanguage") || defaultLanguage; // Get current language
+    const newLang = lang ? lang : "es"; // Toggle language between "es" and "en"
+    setLanguage(newLang); // Set the new language
+  });
 });
+
+// Para expandir y encoger el encabezado al bajar
+
+const shrinkThreshold = 140;
+const expandThreshold = 60;
 
 $(window).on("scroll", function () {
   var windscroll = $(window).scrollTop();
-  if (windscroll >= 100) {
+
+  if (windscroll >= shrinkThreshold) {
     $(".site-navigation").addClass("nav-bg");
-    // document.getElementById("navLogo").style.width = "84px";
-    // document.getElementById("navbar").style.height = "80px";
     document.getElementById("navLinks").style.fontSize = "16px";
-  } else {
+  }
+
+  if (windscroll < expandThreshold) {
     $(".site-navigation").removeClass("nav-bg");
-    // document.getElementById("navLogo").style.width = "178px";
-    // document.getElementById("navbar").style.height = "132px";
     document.getElementById("navLinks").style.fontSize = "22px";
   }
 });
+
+function getCookie(name) {
+  let matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// Function to set a cookie
+function setCookie(name, value, options = {}) {
+  options = {
+    path: "/",
+    ...options,
+  };
+
+  let updatedCookie =
+    encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+// Function to set the language
+function setLanguage(lang) {
+  setCookie("siteLanguage", lang); // Set the language cookie
+}
