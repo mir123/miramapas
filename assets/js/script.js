7b54c8de6ca4a7ec8d9d9ee0c9ea1c5685921fc8
mir123 <mir123@gmail.com>
2024-09-13 17:45:29 -0600

    PhotoSwipe deep zoom

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,45 +50,6 @@ $(document).ready(function () {
     const newLang = lang ? lang : "es"; // Toggle language between "es" and "en"
     setLanguage(newLang); // Set the new language
   });
-
-  const lightbox = new PhotoSwipeLightbox({
-    gallery: ".site-project-single-description", // Or any parent selector that contains your images
-    children: ".lightbox", // This selects all anchors with class 'lightbox' within the gallery
-    pswpModule: PhotoSwipe,
-    // Optional settings
-    // showHideAnimationType: 'fade',
-    // bgOpacity: 0.8,
-  });
-  lightbox.on("uiRegister", function () {
-    lightbox.pswp.ui.registerElement({
-      name: "custom-caption",
-      order: 9,
-      isButton: false,
-      appendTo: "root",
-      html: "Caption text",
-      onInit: (el, pswp) => {
-        lightbox.pswp.on("change", () => {
-          const currSlideElement = lightbox.pswp.currSlide.data.element;
-          let captionHTML = "";
-          if (currSlideElement) {
-            const hiddenCaption =
-              currSlideElement.querySelector(".image-caption");
-            if (hiddenCaption) {
-              // get caption from element with class hidden-caption-content
-              captionHTML = hiddenCaption.innerHTML;
-            } else {
-              // get caption from alt attribute
-              captionHTML = currSlideElement
-                .querySelector("img")
-                .getAttribute("alt");
-            }
-          }
-          el.innerHTML = captionHTML || "";
-        });
-      },
-    });
-  });
-  lightbox.init();
 });
 
 // Para expandir y encoger el encabezado al bajar
